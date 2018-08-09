7d72fa0d829b51e6e75f2f2a8c03422a31d5545e
bibinoo <ze_bibinoo@yahoo.com>
2018-08-09 12:29:45 +0800

    Update .gitignore to remove unecessary folders and files to commit

--- custom_logger.py
+++ custom_logger.py
@@ -17,18 +17,23 @@ def setup_logging(default_path = 'conf/log_conf.yaml', default_level = logging.I
     if os.path.exists(path):
         with open(path, 'rt') as f:
             config = yaml.safe_load(f.read())
+            print(config)
         logging.config.dictConfig(config)
+        logging.info('Logging configration loaded and ready to be used')
     else:
         logging.basicConfig(level=default_level)
 
 
 def do_something():
     logging.info('Doing something')
-
+    logging.debug('Doing something in debug mode')
+    logging.critical('Doing something in critical mode')
+    print(logging.config.dictConfig.__dict__)
 
 def main():
     logging.info('Started')
     logging.debug('Started in debug mode')
+    logging.warning('Started in warning mode')
     do_something()
     logging.info('Finished')
 
