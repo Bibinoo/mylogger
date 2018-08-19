554453795ed80dde5f8d2f3ed73e5daf53717e56
bibinoo <ze_bibinoo@yahoo.com>
2018-08-19 10:40:32 +0800

    Replace value from dict with a class working
    Remain to use the class properly to change the value correctly

--- lib/logger.py
+++ lib/logger.py
@@ -1,17 +1,60 @@
 import os, sys
 import logging.config
 import yaml
+#import curses
 
 print (os.path.dirname(__file__))
 
+#curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_BLACK)
+#stdscr.addstr(0, 0, 'hello', curses.color_pair(1))
+# This class is working but may be dangerous
+class WorkingObjDict():
+    def __init__(self, dictionary=None):
+        if dictionary is None:
+            pass
+        elif isinstance(dictionary, dict):
+            self.__dict__.update(dictionary)
+            #print(self.__dict__)
+        else:
+            raise TypeError('A dictionary is expected as an argument')
+
+    def replace_value(self, key_searched, new_value):
+        for k, value in self.__dict__.items():
+            # Recursive method
+            if isinstance(value, dict):
+                self.__dict__[k] = ObjDict(value).replace_value(key_searched, new_value)
+        if key_searched in self.__dict__:
+            # replace the value
+            self.__dict__[key_searched] = new_value
+        return self.__dict__
+
+class ObjDict(dict):
+    def replace_value(self, key_searched, new_value):
+        for k, value in self.items():
+            if isinstance(value, dict):
+                self[k] = ObjDict(value).replace_value(key_searched, new_value)
+        if key_searched in self:
+            # replace the value
+            self[key_searched] = new_value
+        return self
+
 # Recursively replace dictionary values with matching key
-def replace_dictitem(obj, key, replace_value):
+def replace_dictitem(obj, key, new_value):
     for k, v in obj.items():
         if isinstance(v, dict):
-            obj[k] = replace_dictitem(v, key, replace_value)
+            obj[k] = replace_dictitem(v, key, new_value)
     if key in obj:
         print(obj[key])
-        obj[key] = replace_value
+        # replace the value
+        obj[key] = new_value
+        print(obj[key])
+        # insert a value
+        obj[key] = 'insert value: ' + new_value
+        print(obj[key])
+        # append a value
+        obj[key] = new_value + ' :append value'
+        # delete the key
+        # del obj[key]
         print(obj[key])
     return obj
 
@@ -45,8 +88,10 @@ def setup_logging(default_path = 'conf/', default_logconf = 'log_conf.yaml' ,
             config = yaml.safe_load(file.read())
         logging.config.dictConfig(config)
 
-        replace_dictitem(config, 'filename', 'new path name')
-
+        # Replace the value of the key filename by default path
+        instance = ObjDict(config)
+        config = instance.replace_value('filename', default_path + "\\")
+        print(config)
         logging.getLogger(__name__).info('Loaded ' + logconf_path +
             ' configuration file for logger!')
     else:
