860ca4d54e415439b64f84ce15079e4bd06eeff7
Bibinoo <Bibinoo@users.noreply.github.com>
2020-04-23 14:23:28 +0800

    Delete config.py

--- loggerpkg/tests/config.py
+++
@@ -1,5 +0,0 @@
-# configuration file
-class LOGGER:
-    CONF_FILE = "conf/log_conf.yaml"
-    PATH_FOLDER = "C:\\Users\\denny_000\\Documents\\GitHub\\outlook\\logger"
-    LOGGING_LEVEL = "logging.DEBUG"
